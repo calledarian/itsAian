c396c9b2719ca53d7b1a28f48a0dd17b43240245
calledarian <ariankhadem4@gmail.com>
2025-03-08 15:32:46 +0700

    man see procces, man happy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let contactMeBtns = document.getElementsByClassName("contact-me");
 
 for (let btn of contactMeBtns) {
-    btn.addEventListener("click", function() {
+    btn.addEventListener("click", function () {
 
         let emailBox = document.getElementById("email-box");
 
