a9cd0dfa36488afc50d62946ce93480e3ebbdadb
calledarian <ariankhadem4@gmail.com>
2025-03-11 10:43:33 +0700

    blinking effect to the bio + tooltips for project-box

--- index.js
+++ index.js
@@ -14,21 +14,29 @@ for (let btn of contactMeBtns) {
 }
 
 let profileBio = "Welcome to my portfolio! Here you'll find my projects, skills, and interests as a Junior Developer. Explore and feel free to connect!";
-
-
 let bioElement = document.querySelector(".profile-bio");
 bioElement.innerHTML = "";
 
 let i = 0;
+let cursorVisible = true;
 
 function typeBio() {
-    if (i < profileBio.length) {  // Use profileBio instead of bioText
-        bioElement.innerHTML += profileBio.charAt(i);
+    if (i < profileBio.length) {
+        bioElement.innerHTML = profileBio.substring(0, i) + "<span class='cursor'>|</span>";
         i++;
         setTimeout(typeBio, 50);
+    } else {
+        blinkCursor(); // Start blinking effect after typing finishes
     }
 }
 
+function blinkCursor() {
+    setInterval(() => {
+        cursorVisible = !cursorVisible;
+        document.querySelector(".cursor").style.opacity = cursorVisible ? "1" : "0";
+    }, 500);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    typeBio();  // Starts typing effect when the DOM content is loaded
+    typeBio();
 });
